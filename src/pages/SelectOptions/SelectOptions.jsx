b2d70f9d2ba4bb7c71490557a3987fc4a5cfc9fc
Nicolas Martinez <nicmart@gmail.com>
2024-05-31 12:12:15 -0700

    add print budget component to page and pretty print

--- src/pages/SelectOptions/SelectOptions.jsx
+++ src/pages/SelectOptions/SelectOptions.jsx
@@ -1,9 +1,11 @@
 import SelectGenres from "../../components/SelectGenres/SelectGenres";
+import SelectBudget from "../../components/SelectBudget/SelectBudget";
 
 function SelectOptions() {
   return (
     <>
       {/* TODO: Insert Step 1 budget component */}
+      <SelectBudget />
       <SelectGenres />
       {/* Add other steps components */}
     </>
