505ea215f5c646cdbb428fafb91a980146ecab1a
ergaganjit <er.gaganjit.kaur@gmail.com>
2024-05-31 09:01:31 -0700

    Added header and required folders for that

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './components/header/header';
+
+
+const App = () => {
+    return (
+        <div>
+            <Header />
+            {/* Other components */}
+         
+        </div>
+    );
+};
+
+export default App;
