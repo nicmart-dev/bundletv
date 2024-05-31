c71a9e6091a9b15eee7e748912e5e0441527c576
Abenezer Chane <abenezerchane@gmail.com>
2024-05-31 13:35:26 -0700

    select channels

--- src/pages/SelectOptions/SelectOptions.jsx
+++ src/pages/SelectOptions/SelectOptions.jsx
@@ -1,6 +1,7 @@
 import SelectGenres from "../../components/SelectGenres/SelectGenres";
 import SelectBudget from "../../components/SelectBudget/SelectBudget";
 import SelectKids from '../../components/SelectKids/SelectKids';
+import SelectChannels from "../../components/SelectChannels/SelectChannels";
 
 function SelectOptions() {
   return (
@@ -8,6 +9,7 @@ function SelectOptions() {
       {/* TODO: Insert Step 1 budget component */}
       <SelectBudget />
       <SelectGenres />
+      <SelectChannels />
       <SelectKids />
       {/* Add other steps components */}
     </>
