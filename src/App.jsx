cc9cdff67ad375e73a4782a24481f118f2e94a6a
Nicolas Martinez <nicmart@gmail.com>
2024-05-31 12:00:58 -0700

    cleanup App component to reduce merge conflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import React from 'react';
-import Header from './components/Header/Header';
-
+import React from "react";
+import Header from "./components/Header/Header";
+import SelectOptions from "./pages/SelectOptions/SelectOptions";
+import "./App.scss";
 
 const App = () => {
-    return (
-        <div>
-            <Header />
-            {/* Other components */}
-         
-        </div>
-    );
+  return (
+    <div>
+      <Header />
+      <SelectOptions />
+      {/* Other components */}
+    </div>
+  );
 };
 
 export default App;
