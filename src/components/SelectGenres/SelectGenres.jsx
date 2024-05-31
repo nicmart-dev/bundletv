5f39258ba9124d454dccf7120f58a83a326c7896
Nicolas Martinez <nicmart@gmail.com>
2024-05-31 12:53:28 -0700

    add json genres list from imdb

--- src/components/SelectGenres/SelectGenres.jsx
+++ src/components/SelectGenres/SelectGenres.jsx
@@ -1,17 +1,6 @@
 import React from "react";
 import MultiSelect from "../MultiSelect/MultiSelect";
-
-const genres = [
-  { value: "action", label: "Action" },
-  { value: "comedy", label: "Comedy" },
-  { value: "drama", label: "Drama" },
-  { value: "fantasy", label: "Fantasy" },
-  { value: "horror", label: "Horror" },
-  { value: "mystery", label: "Mystery" },
-  { value: "romance", label: "Romance" },
-  { value: "sci-fi", label: "Sci-Fi" },
-  { value: "thriller", label: "Thriller" },
-];
+import genres from "../../data/genres.json"; // Import the JSON file
 
 const SelectGenres = () => {
   const handleGenreChange = (selectedGenres) => {
