177ad2f2649b4057e2ae70b3aa22aaffb335061f
Sara Chang <sara@Avocado.lan>
2024-05-31 15:17:23 -0700

    scroll to top

--- src/components/ButtonBundle/ButtonBundle.jsx
+++ src/components/ButtonBundle/ButtonBundle.jsx
@@ -1,17 +1,18 @@
-import './ButtonBundle.scss';
-import {Link} from 'react-router-dom';
+import "./ButtonBundle.scss";
+import { Link } from "react-router-dom";
 
 const ButtonBundle = () => {
+  const handleClick = () => {
+    window.scrollTo(0, 0);
+  };
 
-    return (
-        
-        <div className="ButtonBundle">
-            <Link to='/package'>
-            <button className="ButtonBundle__btn">Create Your Bundle</button>
-            </Link>
-        </div>
-     
-    )
-}
+  return (
+    <div className="ButtonBundle">
+      <Link to="/package" onClick={handleClick}>
+        <button className="ButtonBundle__btn">Create Your Bundle</button>
+      </Link>
+    </div>
+  );
+};
 
 export default ButtonBundle;
