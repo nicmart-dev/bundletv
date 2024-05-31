8191836427959fd282f87392f155baeef43cddd7
Daksh Kashyap <65839160+dakshkashyap@users.noreply.github.com>
2024-05-31 13:37:44 -0700

    added hero1 and hero2 in their respective pages

--- src/pages/SelectOptions/SelectOptions.jsx
+++ src/pages/SelectOptions/SelectOptions.jsx
@@ -1,10 +1,12 @@
 import SelectGenres from "../../components/SelectGenres/SelectGenres";
 import SelectBudget from "../../components/SelectBudget/SelectBudget";
+import Hero from "../../components/hero1/hero1"
 
 function SelectOptions() {
   return (
     <>
       {/* TODO: Insert Step 1 budget component */}
+      <Hero />
       <SelectBudget />
       <SelectGenres />
       {/* Add other steps components */}
