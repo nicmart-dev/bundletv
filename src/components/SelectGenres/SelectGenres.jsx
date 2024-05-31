ecbccd0684fea5ff1ff81050e2e9401ced7f1d86
Nicolas Martinez <nicmart@gmail.com>
2024-05-31 13:26:42 -0700

    add genres slicer styling consistent with budget slicer

--- src/components/SelectGenres/SelectGenres.jsx
+++ src/components/SelectGenres/SelectGenres.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import MultiSelect from "../MultiSelect/MultiSelect";
 import genres from "../../data/genres.json"; // Import the JSON file
+import genresIcon from "../../assets/icons/big-drama.svg";
+import "./SelectGenres.scss";
 
 const SelectGenres = () => {
   const handleGenreChange = (selectedGenres) => {
@@ -8,13 +10,19 @@ const SelectGenres = () => {
   };
 
   return (
-    <>
+    <div className="genres">
+      <p className="genres__step">STEP 2</p>
+      <img src={genresIcon} alt="budget-icon" className="genres__icon" />
+      <h2 className="genres__title">Genres</h2>
+      <p className="genres__description">
+        Tell us what genres you enjoy watching.
+      </p>
       <MultiSelect
         options={genres}
         placeholder="Select genres..."
         onChange={handleGenreChange}
       />
-    </>
+    </div>
   );
 };
 export default SelectGenres;
